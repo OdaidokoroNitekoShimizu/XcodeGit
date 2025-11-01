a6747a78e48cc95ca01a48d0d4652f505e1f9931
Hiroyuki <lahm0107@gmail.com>
2025-11-01 18:14:48 +0900

    Initial Commit

---
+++ XcodeGit/XcodeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitApp.swift
+//  XcodeGit
+//
+//  Created by hiroyuki takahashi on R 7/11/01.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
